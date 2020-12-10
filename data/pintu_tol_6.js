85aa6606754d6ec021826f1d3ea0cd0afd75eb03
ardiansyahgeo <67992886+ardiansyahgeo@users.noreply.github.com>
2020-12-10 09:05:44 +0700

    Add files via upload

---
+++ data/pintu_tol_6.js
@@ -0,0 +1,11 @@
+var json_pintu_tol_6 = {
+"type": "FeatureCollection",
+"name": "pintu_tol_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OBJECTID": "1" }, "geometry": { "type": "Point", "coordinates": [ 105.202829693689765, -5.158388688708065 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "2" }, "geometry": { "type": "Point", "coordinates": [ 105.210210438266486, -5.277376598699805 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "3" }, "geometry": { "type": "Point", "coordinates": [ 105.324151114205534, -5.352424323341889 ] } },
+{ "type": "Feature", "properties": { "OBJECTID": "4" }, "geometry": { "type": "Point", "coordinates": [ 105.348247043131423, -5.397351042218669 ] } }
+]
+}
